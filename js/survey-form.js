9637109e7987558f72277a1ec8e1db9d64a2168a
Daniel Larbi Addo <danieladdo242@gmail.com>
2019-03-16 16:36:37 -0800

    done

--- js/survey-form.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('input[type="checkbox"]').on('click', function() {
-        var values = [];
-        $('input:checked').each(function() {
-            values.push($(this).parent().text());
-        });
-        $('[name="'+$(this).attr("data-input")+'"]').attr({value: values.join(', ')});
-    });
-});
